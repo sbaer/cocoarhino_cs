110941d1c00f24b563eea30cc98f3ddee8b598b0
sbaer <steve@mcneel.com>
2012-12-31 16:53:15 -0800

    added WPF sample that works the same as the OSX plug-in

---
+++ wpf_sample/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace wpf_checkbox_sample
+{
+  public class RelayCommand : ICommand
+  {
+    readonly Func<Boolean> _canExecute;
+    readonly Action _execute;
+
+    public RelayCommand(Action execute)
+      : this(execute, null)
+    {
+    }
+
+    public RelayCommand(Action execute, Func<Boolean> canExecute)
+    {
+      if (execute == null)
+        throw new ArgumentNullException("execute");
+      _execute = execute;
+      _canExecute = canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+      add
+      {
+        if (_canExecute != null)
+          CommandManager.RequerySuggested += value;
+      }
+      remove
+      {
+
+        if (_canExecute != null)
+          CommandManager.RequerySuggested -= value;
+      }
+    }
+
+    public Boolean CanExecute(Object parameter)
+    {
+      return _canExecute == null ? true : _canExecute();
+    }
+
+    public void Execute(Object parameter)
+    {
+      _execute();
+    }
+  }
+}
