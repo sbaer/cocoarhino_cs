75680379587aa0862886581037d532bcc9dc94fa
sbaer <steve@mcneel.com>
2013-01-02 11:14:23 -0800

    Going to start piecing together RhinoMac

--- UnsafeNativeMethods.cs
+++ UnsafeNativeMethods.cs
@@ -18,18 +18,30 @@ namespace CocoaRhino_CS
 
     [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
     public static extern void RUI_RegisterActionCallback (ViewModelBase.PerformActionCallback actionFunc);
-    
+
+    [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RUI_RegisterWindowWillCloseCallback(ViewModelBase.WindowWillCloseCallback willCloseFunc);
+
+    [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RUI_RegisterPropertyTypeCallback(ViewModelBase.PropertyTypeCallback propertyTypeFunc);
+
+    [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RUI_RegisterActionExistsCallback(ViewModelBase.ActionExistsCallback actionExistsFunc);
+
     [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr RUI_CreateWindow([MarshalAs(UnmanagedType.LPWStr)]string nib);
-    
+
+    [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RUI_ValueChanged(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string value);
+
     [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
     public static extern void RUI_ShowModalWindow(IntPtr controller);
     
     [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
     public static extern void ShowWindow(IntPtr handle);
-    
+
     [DllImport(nativelib, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void RUI_ValueChanged(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string value);
+    public static extern IntPtr RUI_GetWindow(IntPtr createWindowHandle);
   }
 }
 
--- ViewModelBase.cs
+++ ViewModelBase.cs
@@ -15,6 +15,9 @@ namespace CocoaRhino_CS
       UnsafeNativeMethods.RUI_RegisterBoolCallbacks(m_getbool_callback, m_setbool_callback);
       UnsafeNativeMethods.RUI_RegisterStringCallbacks(m_getstring_callback, m_setstring_callback);
       UnsafeNativeMethods.RUI_RegisterActionCallback(m_perform_action);
+      UnsafeNativeMethods.RUI_RegisterWindowWillCloseCallback(m_willclose_callback);
+      UnsafeNativeMethods.RUI_RegisterPropertyTypeCallback(m_propertytype_callback);
+      UnsafeNativeMethods.RUI_RegisterActionExistsCallback(m_actionexists_callback);
     }
 
     public string WindowName { get; private set; }
@@ -64,21 +67,27 @@ namespace CocoaRhino_CS
     internal delegate string GetStringValueCallback(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string name);
     internal delegate void SetStringValueCallback(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string name, [MarshalAs(UnmanagedType.LPWStr)]string value);
     internal delegate void PerformActionCallback(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string name);
-    
-    
+    internal delegate void WindowWillCloseCallback(IntPtr handle);
+    internal delegate int PropertyTypeCallback(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string name);
+    internal delegate int ActionExistsCallback(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)]string name);
+
+
     static Dictionary<IntPtr, ViewModelBase> m_all_controllers = new Dictionary<IntPtr, ViewModelBase>();
     static GetBoolValueCallback m_getbool_callback = GetBoolCalledFromC;
     static SetBoolValueCallback m_setbool_callback = SetBoolCalledFromC;
     static GetStringValueCallback m_getstring_callback = GetStringCalledFromC;
     static SetStringValueCallback m_setstring_callback = SetStringCalledFromC;
     static PerformActionCallback m_perform_action = PerformActionCalledFromC;
+    static WindowWillCloseCallback m_willclose_callback = WindowWillCloseCalledFromC;
+    static PropertyTypeCallback m_propertytype_callback = PropertyTypeCalledFromC;
+    static ActionExistsCallback m_actionexists_callback = ActionExistsCalledFromC;
     
-    static int GetBoolCalledFromC(IntPtr handle, string name)
+    static int GetBoolCalledFromC(IntPtr pController, string name)
     {
       int rc = 0;
-      if( m_all_controllers.ContainsKey(handle) )
+      if( m_all_controllers.ContainsKey(pController) )
       {
-        var item = m_all_controllers[handle];
+        var item = m_all_controllers[pController];
         var prop = item.GetType().GetProperty(name);
         if( prop!=null )
         {
@@ -89,12 +98,12 @@ namespace CocoaRhino_CS
       return rc;
     }
     
-    static string GetStringCalledFromC(IntPtr handle, string name)
+    static string GetStringCalledFromC(IntPtr pController, string name)
     {
       string rc = null;
-      if( m_all_controllers.ContainsKey(handle) )
+      if( m_all_controllers.ContainsKey(pController) )
       {
-        var item = m_all_controllers[handle];
+        var item = m_all_controllers[pController];
         var prop = item.GetType().GetProperty(name);
         if( prop!=null )
         {
@@ -104,39 +113,82 @@ namespace CocoaRhino_CS
       return rc;
     }
 
-    static void SetBoolCalledFromC(IntPtr handle, string name, int value)
+    static void SetBoolCalledFromC(IntPtr pController, string name, int value)
     {
-      if( m_all_controllers.ContainsKey(handle) )
+      if( m_all_controllers.ContainsKey(pController) )
       {
-        var item = m_all_controllers[handle];
+        var item = m_all_controllers[pController];
         var prop = item.GetType().GetProperty(name);
         if( prop!=null )
           prop.SetValue(item, value!=0, null);
       }
     }
     
-    static void SetStringCalledFromC(IntPtr handle, string name, string value)
+    static void SetStringCalledFromC(IntPtr pController, string name, string value)
     {
-      if( m_all_controllers.ContainsKey(handle) )
+      if( m_all_controllers.ContainsKey(pController) )
       {
-        var item = m_all_controllers[handle];
+        var item = m_all_controllers[pController];
         var prop = item.GetType().GetProperty(name);
         if( prop!=null )
           prop.SetValue(item, value, null);
       }
     }
 
-    static void PerformActionCalledFromC(IntPtr handle, string name)
+    static void PerformActionCalledFromC(IntPtr pController, string name)
     {
-      if( m_all_controllers.ContainsKey(handle) )
+      if( m_all_controllers.ContainsKey(pController) )
       {
-        var item = m_all_controllers[handle];
+        var item = m_all_controllers[pController];
         var method = item.GetType().GetMethod(name);
         if( method!=null )
           method.Invoke(item, null);
       }
     }
 
+    static void WindowWillCloseCalledFromC(IntPtr pController)
+    {
+      m_all_controllers.Remove(pController);
+    }
+
+    enum PropertyType : int
+    {
+      ruitypePropertyDoesNotExist = -1,
+      ruitypeUnknown = 0,
+      ruitypeBoolean,
+      ruitypeString,
+    }
+    static int PropertyTypeCalledFromC(IntPtr pController, string name)
+    {
+      PropertyType rc = PropertyType.ruitypePropertyDoesNotExist;
+      if( m_all_controllers.ContainsKey(pController) )
+      {
+        var item = m_all_controllers[pController];
+        var prop = item.GetType().GetProperty(name);
+        if( prop!=null )
+        {
+          rc = PropertyType.ruitypeUnknown;
+          if( prop.PropertyType == typeof(bool) )
+            rc = PropertyType.ruitypeBoolean;
+          else if(prop.PropertyType == typeof(string) )
+            rc = PropertyType.ruitypeString;
+        }
+      }
+      return (int)rc;
+    }
+
+    static int ActionExistsCalledFromC(IntPtr pController, string name)
+    {
+      int rc = 0;
+      if( m_all_controllers.ContainsKey(pController) )
+      {
+        var item = m_all_controllers[pController];
+        var method = item.GetType().GetMethod(name);
+        if( method!=null )
+          rc = 1;
+      }
+      return rc;
+    }
   }
 }
 
