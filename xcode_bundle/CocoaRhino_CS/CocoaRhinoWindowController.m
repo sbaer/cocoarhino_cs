d63526aeb30cbe685d3d517ccae3deeec704f565
sbaer <steve@mcneel.com>
2013-02-22 09:11:17 -0800

    Experimenting with custom window controllers

---
+++ xcode_bundle/CocoaRhino_CS/CocoaRhinoWindowController.m
@@ -0,0 +1,37 @@
+//
+//  CocoaRhinoWindowController.m
+//  CocoaRhino_CS
+//
+//  Created by Steve Baer on 2/22/13.
+//  Copyright (c) 2013 Steven Baer. All rights reserved.
+//
+
+#import "CocoaRhinoWindowController.h"
+/*
+@interface CocoaRhinoWindowController ()
+
+@end
+
+@implementation CocoaRhinoWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+- (IBAction)ButtonClick:(id)sender {
+}
+@end
+*/
