781dea66889320ff44b090da2088540b16112d0c
sbaer <steve@mcneel.com>
2013-03-08 15:24:55 -0800

    messing around with custom button

--- CocoaRhinoCommand.cs
+++ CocoaRhinoCommand.cs
@@ -12,7 +12,7 @@ namespace CocoaRhino_CS
       var win = RhinoMac.RhinoWindow.FromNib("CocoaRhinoWindow", vm);
       win.Title = "On the fly";
       var rect = new System.Drawing.RectangleF(10, 100, 200, 50);
-      var btn = new MonoMac.AppKit.NSButton(rect);
+			var btn = new CustomButton (rect);// MonoMac.AppKit.NSButton(rect);
 
       win.ContentView.AddSubview(btn);
       btn.Title = "On the fly";
@@ -24,5 +24,20 @@ namespace CocoaRhino_CS
       return Rhino.Commands.Result.Success;
     }
   }
+
+	class CustomButton : MonoMac.AppKit.NSButton
+	{
+		public CustomButton(System.Drawing.RectangleF r):base(r){
+		}
+
+		public override void DrawRect (MonoMac.Foundation.NSRect dirtyRect)
+		{
+			base.DrawRect (dirtyRect);
+			var context = MonoMac.AppKit.NSGraphicsContext.CurrentContext.GraphicsPort;
+			context.SetFillColor (new MonoMac.CoreGraphics.CGColor (1, 0, 0));
+			context.FillRect (new System.Drawing.RectangleF (5, 5, 10, 10));
+			//base.DrawRect (dirtyRect);
+		}
+	}
 }
 
