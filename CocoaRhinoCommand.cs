09721a695b527b2962a7dc56b01e2eed67793676
sbaer <steve@mcneel.com>
2013-04-06 14:28:25 -0700

    latest experiments with font manager

--- CocoaRhinoCommand.cs
+++ CocoaRhinoCommand.cs
@@ -8,25 +8,44 @@ namespace CocoaRhino_CS
 
     protected override Rhino.Commands.Result RunCommand (Rhino.RhinoDoc doc, Rhino.Commands.RunMode mode)
     {
+      MonoMac.ObjCRuntime.Runtime.RegisterAssembly (this.GetType ().Assembly);
       var vm = new DNViewModel();
       var win = RhinoMac.RhinoWindow.FromNib("CocoaRhinoWindow", vm);
+
       win.Title = "On the fly";
 
       var rect = new System.Drawing.RectangleF(10, 100, 200, 50);
       //var btn = new MonoMac.AppKit.NSButton (rect);
 			var btn = new CustomButton(rect);
+
       win.ContentView.AddSubview(btn);
       btn.Title = "On the fly";
       btn.SetButtonType(MonoMac.AppKit.NSButtonType.MomentaryLightButton);
       btn.BezelStyle = MonoMac.AppKit.NSBezelStyle.Rounded;
-      btn.Activated += (object sender, EventArgs e) => Rhino.RhinoApp.WriteLine("I got click!!!");
+      TestResponer res = new TestResponer ();
+      btn.Activated += (object sender, EventArgs e) => {
+        MonoMac.AppKit.NSFontManager.SharedFontManager.Target = res;
+        MonoMac.AppKit.NSFontManager.SharedFontManager.OrderFrontFontPanel (btn);
+      };
+      res.NextResponder = win.NextResponder;
+      win.NextResponder = res;
+
       win.ShowModal();
 
       return Rhino.Commands.Result.Success;
     }
   }
 
-  class CustomButton : MonoMac.AppKit.NSButton
+  public class TestResponer : MonoMac.AppKit.NSResponder
+  {
+    [MonoMac.Foundation.Export("changeFont:")]
+    public void ChangeFont(MonoMac.Foundation.NSObject sender)
+    {
+      Rhino.RhinoApp.WriteLine ("YES");
+    }
+  }
+
+  public class CustomButton : MonoMac.AppKit.NSButton
 	{
 		public CustomButton(System.Drawing.RectangleF r):base(r){
 		}
